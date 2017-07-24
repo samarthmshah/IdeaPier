9b7b695a4f896467527fc4b1f97ab57287856f22
coproductions <constantin.dumba@gmail.com>
2017-07-24 15:56:59 -0700

    updates users

--- models/user.js
+++ models/user.js
@@ -4,8 +4,9 @@ module.exports = (function userSchema () {
   var mongoose = require('../db').mongoose;
  
   var schema = {
-    username: {type: String, required: false},
+    username: {type: String, required: true},
     topics: {type: Array, required: false},
+    channels: {type: Array, required: false}
   };
   var collectionName = 'users';
   var userSchema = new mongoose.Schema(schema);
