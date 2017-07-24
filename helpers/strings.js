b5d2bd34c70f9f280acc5f96ebd9a174a367b715
coproductions <constantin.dumba@gmail.com>
2017-07-24 14:12:38 -0700

    adds string dict

---
+++ helpers/strings.js
@@ -0,0 +1,5 @@
+export const strings = {
+    getGreeting: firstName => `Hi ${firstName}`
+    // add other greeting and strings here with name annd topic params
+    // TODO add synonym finder here
+};
