cc9bb57019e9abc573eb8848072998a72604231a
Molly Kent <kent.molly@gmail.com>
2017-07-25 13:01:57 -0700

    add route for finding single user

--- routes.js
+++ routes.js
@@ -3,10 +3,10 @@ module.exports = function(app) {
   const topic = require('./controllers/topicController');
 
   app.post('/createUser', user.createUser); 
-  app.get('/getUsers', user.getUser);  
+  app.get('/getUsers', user.getUsers);  
 
   app.post('/createTopic', topic.createTopicHttp);
-  app.get('/getTopic/:topic', topic.getTopic);
+  app.get('/getTopic/:topic', topic.getTopicHttp);
   app.get('/getTopicsBySyn/:topic', topic.getTopicsBySynHttp);
 
   app.get('/', function(req, res, next) {
